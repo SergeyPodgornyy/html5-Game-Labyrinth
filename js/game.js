be17bf4fb67f22ebd92a1a4e76d1ebcf68495398
Sergey Podgornyy <sergey.podgornyy@yahoo.de>
2015-10-20 22:37:03 +0300

    Uploading Mike The Frog and request Animation Frame

--- js/game.js
+++ js/game.js
@@ -1,2 +1,20 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+
+imgFrog = new Image();
+imgFrog.src = "img/mikethefrog.png";
+imgFrog.addEventListener("load",init,false);
+
+var requestAnimFrame = 
+				window.requestAnimationFrame ||
+				window.webkitRequestAnimationFrame ||
+				window.mozReqeustAnimationFrame ||
+				window.oReqeustAnimationFrame ||
+				window.msReqeustAnimationFrame ||
+				function (callback){
+					window.setTimeout(callback, 1000/60);
+				};
+
+function init(){
+
+}
